ddb3dac0f321700cbb2deac87671b38a34f948e5
William758 <williamreyes758@gmail.com>
2021-01-14 01:51:18 -0800

    Improve item description and hooks

--- ZetAspectFire.cs
+++ ZetAspectFire.cs
@@ -14,16 +14,15 @@ namespace TPDespair.ZetAspects
 		internal static void Init()
 		{
 			DefineItem();
-			SetHook();
+			MovespeedHook();
+			JumpHook();
+			FiretrailHook();
+			BurnDelimiterHook();
+			BurnApplicationHook();
 		}
 
 		private static void DefineItem()
 		{
-			string st1 = ZetAspectsPlugin.ZetAspectRedBurnDurationCfg.Value == 1.0f ? "" : "s";
-			string lim = ZetAspectsPlugin.ZetAspectRedMovementGainCapCfg.Value > 0f ? " up to <style=cIsUtility>" + (ZetAspectsPlugin.ZetAspectRedMovementGainCapCfg.Value * 100f) + "%</style>" : "";
-			string trail = ZetAspectsPlugin.ZetAspectRedTrailCfg.Value ? "\nLeave behind a fiery trail that damages enemies on contact." : "";
-			string jump = ZetAspectsPlugin.ZetAspectRedExtraJumpCfg.Value ? "\nGain <style=cIsUtility>+1</style> maximum <style=cIsUtility>jump count</style>." : "";
-
 			ItemTag[] tags = { ItemTag.Damage, ItemTag.Utility };
 
 			if (!ZetAspectsPlugin.ZetAspectRedTierCfg.Value)
@@ -40,7 +39,7 @@ namespace TPDespair.ZetAspects
 				pickupIconPath = "Textures/ItemIcons/texAffixRedIcon",
 				nameToken = "Ifrit's Distinction",
 				pickupToken = "Become an aspect of fire.",
-				descriptionToken = "<style=cDeath>Aspect of Fire</style> :" + trail + jump + "\nIncreases <style=cIsUtility>movement speed</style> by <style=cIsUtility>" + (ZetAspectsPlugin.ZetAspectRedBaseMovementGainCfg.Value*100f) + "%</style> <style=cStack>(+" + (ZetAspectsPlugin.ZetAspectRedStackMovementGainCfg.Value * 100f) + "% per stack)</style>" + lim + ".\nAttacks <style=cIsDamage>burn</style> on hit for <style=cIsDamage>" + (ZetAspectsPlugin.ZetAspectRedBaseDamageCfg.Value * 100f) + "%</style> <style=cStack>(+" + (ZetAspectsPlugin.ZetAspectRedStackDamageCfg.Value * 100f) + "% per stack)</style> TOTAL damage over "+ ZetAspectsPlugin.ZetAspectRedBurnDurationCfg.Value + " second"+ st1 + ".",
+				descriptionToken = BuildDescription(),
 				loreToken = "Become an aspect of fire.",
 				tags = tags
 			};
@@ -50,9 +49,197 @@ namespace TPDespair.ZetAspects
 			itemIndex = ItemAPI.Add(new CustomItem(itemDef, disp));
 		}
 
-		private static void SetHook()
+		public static string BuildDescription()
+        {
+			string output = "<style=cDeath>Aspect of Fire</style> :";
+			if (ZetAspectsPlugin.ZetAspectRedTrailCfg.Value)
+			{
+				output += "\nLeave behind a fiery trail that damages enemies on contact.";
+			}
+			if (ZetAspectsPlugin.ZetAspectRedExtraJumpCfg.Value)
+			{
+				output += "\nGain <style=cIsUtility>+1</style> maximum <style=cIsUtility>jump count</style>.";
+			}
+			if (ZetAspectsPlugin.ZetAspectRedBaseMovementGainCfg.Value > 0f)
+			{
+				output += "\nIncreases <style=cIsUtility>movement speed</style> by <style=cIsUtility>";
+				output += ZetAspectsPlugin.ZetAspectRedBaseMovementGainCfg.Value * 100f + "%</style>";
+				if (ZetAspectsPlugin.ZetAspectRedStackMovementGainCfg.Value != 0f)
+				{
+					output += " <style=cStack>(+";
+					output += ZetAspectsPlugin.ZetAspectRedStackMovementGainCfg.Value * 100f + "% per stack)</style>";
+				}
+				if (ZetAspectsPlugin.ZetAspectRedMovementGainCapCfg.Value > 0f)
+				{
+					output += " up to <style=cIsUtility>";
+					output += ZetAspectsPlugin.ZetAspectRedMovementGainCapCfg.Value * 100f + "%</style>";
+				}
+				output += ".";
+			}
+			if (ZetAspectsPlugin.ZetAspectRedBaseDamageCfg.Value > 0f)
+			{
+				output += "\nAttacks <style=cIsDamage>burn</style> on hit for <style=cIsDamage>";
+				output += ZetAspectsPlugin.ZetAspectRedBaseDamageCfg.Value * 100f + "%</style>";
+				if (ZetAspectsPlugin.ZetAspectRedStackDamageCfg.Value != 0f)
+				{
+					output += " <style=cStack>(+";
+					output += ZetAspectsPlugin.ZetAspectRedStackDamageCfg.Value * 100f + "% per stack)</style>";
+				}
+				output += ZetAspectsPlugin.ZetAspectRedUseBaseCfg.Value ? " base" : " TOTAL";
+				output += " damage over ";
+				output += ZetAspectsPlugin.FormatSeconds(ZetAspectsPlugin.ZetAspectRedBurnDurationCfg.Value) + ".";
+			}
+
+			return output;
+        }
+
+		private static void MovespeedHook()
 		{
-			// Seperate burn and ignite
+			IL.RoR2.CharacterBody.RecalculateStats += (il) =>
+			{
+				ILCursor c = new ILCursor(il);
+
+				bool found = c.TryGotoNext(
+					x => x.MatchLdloc(53),
+					x => x.MatchLdloc(54),
+					x => x.MatchLdloc(55),
+					x => x.MatchDiv(),
+					x => x.MatchMul(),
+					x => x.MatchStloc(53)
+				);
+
+				if (found)
+				{
+					c.Index += 1;
+
+					c.Emit(OpCodes.Pop);
+
+					c.Emit(OpCodes.Ldarg, 0);
+					c.Emit(OpCodes.Ldloc, 54);
+					c.EmitDelegate<Func<CharacterBody, float, float>>((self, value) =>
+					{
+						if (self.HasBuff(BuffIndex.AffixRed) && ZetAspectsPlugin.ZetAspectRedBaseMovementGainCfg.Value > 0f)
+						{
+							float count = ZetAspectsPlugin.GetStackMagnitude(self, itemIndex);
+							float cap = ZetAspectsPlugin.ZetAspectRedMovementGainCapCfg.Value;
+							float addedSpeed = ZetAspectsPlugin.ZetAspectRedBaseMovementGainCfg.Value + ZetAspectsPlugin.ZetAspectRedStackMovementGainCfg.Value * (count - 1f);
+							if (cap > 0f) addedSpeed = Mathf.Min(addedSpeed, cap);
+							//if (self.teamComponent.teamIndex != TeamIndex.Player) addedSpeed *= 0.5f;
+							value += addedSpeed;
+						}
+
+						return value;
+					});
+					c.Emit(OpCodes.Stloc, 54);
+
+					c.Emit(OpCodes.Ldloc, 53);
+				}
+				else
+				{
+					Debug.LogWarning("ZetAspect : Fire - Movespeed Hook Failed");
+				}
+			};
+		}
+
+		private static void JumpHook()
+		{
+			IL.RoR2.CharacterBody.RecalculateStats += (il) =>
+			{
+				ILCursor c = new ILCursor(il);
+
+				bool found = c.TryGotoNext(
+					x => x.MatchLdarg(0),
+					x => x.MatchLdarg(0),
+					x => x.MatchLdfld<CharacterBody>("baseJumpCount"),
+					x => x.MatchLdloc(5)
+				);
+
+				if (found)
+				{
+					c.Emit(OpCodes.Ldarg, 0);
+					c.Emit(OpCodes.Ldloc, 5);
+					c.EmitDelegate<Func<CharacterBody, int, int>>((self, value) =>
+					{
+						if (self.HasBuff(BuffIndex.AffixRed) && ZetAspectsPlugin.ZetAspectRedExtraJumpCfg.Value)
+						{
+							if (self.teamComponent.teamIndex == TeamIndex.Player) value++;
+						}
+
+						return value;
+					});
+					c.Emit(OpCodes.Stloc, 5);
+				}
+				else
+				{
+					Debug.LogWarning("ZetAspect : Fire - Jump Hook Failed");
+				}
+			};
+		}
+
+		private static void FiretrailHook()
+		{
+			IL.RoR2.CharacterBody.UpdateFireTrail += (il) =>
+			{
+				ILCursor c = new ILCursor(il);
+
+				bool found = c.TryGotoNext(
+					x => x.MatchStloc(0)
+				);
+
+				if (found)
+				{
+					c.Index += 1;
+
+					c.Emit(OpCodes.Ldarg_0);
+					c.Emit(OpCodes.Ldloc, 0);
+					c.EmitDelegate<Func<CharacterBody, bool, bool>>((self, hasBuff) =>
+					{
+						if (self.teamComponent.teamIndex == TeamIndex.Player && !ZetAspectsPlugin.ZetAspectRedTrailCfg.Value) return false;
+
+						return hasBuff;
+					});
+					c.Emit(OpCodes.Stloc, 0);
+				}
+				else
+				{
+					Debug.LogWarning("ZetAspect : Fire - Firetrail Hook Failed");
+				}
+			};
+		}
+
+		private static void BurnDelimiterHook()
+		{
+			IL.RoR2.DotController.AddDot += (il) =>
+			{
+				ILCursor c = new ILCursor(il);
+
+				bool found = c.TryGotoNext(
+					x => x.MatchCallvirt<HealthComponent>("get_fullCombinedHealth"),
+					x => x.MatchLdcR4(0.01f),
+					x => x.MatchMul(),
+					x => x.MatchCallOrCallvirt<Mathf>("Min")
+				);
+
+				if (found)
+				{
+					c.Index += 2;
+
+					c.Emit(OpCodes.Pop);
+					c.Emit(OpCodes.Ldloc_0);
+					c.EmitDelegate<Func<TeamIndex, float>>((teamIndex) =>
+					{
+						return teamIndex == TeamIndex.Player ? 1f : 0.01f;
+					});
+				}
+				else
+				{
+					Debug.LogWarning("ZetAspect : Fire - Burn Delimiter Hook Failed");
+				}
+			};
+		}
+
+		private static void BurnApplicationHook()
+		{
 			IL.RoR2.GlobalEventManager.OnHitEnemy += (il) =>
 			{
 				ILCursor c = new ILCursor(il);
@@ -84,53 +271,57 @@ namespace TPDespair.ZetAspects
 							DotController.InflictDot(victim, damageInfo.attacker, DotController.DotIndex.Burn, 4f * damageInfo.procCoefficient, 1f);
 						}
 
-						// Percent Ignite
-						if (self.HasBuff(BuffIndex.AffixRed))
+						// Fire Aspect Percent Burn
+						if (self.HasBuff(BuffIndex.AffixRed) && ZetAspectsPlugin.ZetAspectRedBaseDamageCfg.Value > 0f)
 						{
-							float baseDuration = ZetAspectsPlugin.ZetAspectRedBurnDurationCfg.Value;
+							float count = ZetAspectsPlugin.GetStackMagnitude(self, itemIndex);
+							float aspectDamageMult = ZetAspectsPlugin.ZetAspectRedBaseDamageCfg.Value + ZetAspectsPlugin.ZetAspectRedStackDamageCfg.Value * (count - 1f);
+							float aspectDuration = ZetAspectsPlugin.ZetAspectRedBurnDurationCfg.Value;
 
-							float igniteDamage = 0f;
-							float aspectDamage = 0f;
+							float ignitePercentBaseDamage = 0f;
+							float aspectPercentBaseDamage = 0f;
 
 							if ((damageInfo.damageType & DamageType.PercentIgniteOnHit) != DamageType.Generic)
 							{
-							// base damage over duration / 5 ticks
-							igniteDamage = 4f * damageInfo.procCoefficient;
+								ignitePercentBaseDamage = 4f * damageInfo.procCoefficient * 0.5f;
 							}
 
-							float damage = Util.OnHitProcDamage(damageInfo.damage, self.damage, 1f);
-							damage /= self.damage;// Multiplied in DotController
-							damage *= 2;// DotDef Interval / Coefficient
-							damage /= baseDuration;
+							if (ZetAspectsPlugin.ZetAspectRedUseBaseCfg.Value)
+							{
+								aspectPercentBaseDamage = aspectDamageMult;
+							}
+							else
+							{
+								aspectPercentBaseDamage = damageInfo.damage / self.damage;
+								aspectPercentBaseDamage *= aspectDamageMult;
+								if (damageInfo.crit) aspectPercentBaseDamage *= GetCritMult(self);
+							}
 
-							// crit damage multiplier
-							damage *= GetCritMult(self, damageInfo);
-
-							float count = ZetAspectsPlugin.GetStackMagnitude(self, itemIndex);
-							damage *= ZetAspectsPlugin.ZetAspectRedBaseDamageCfg.Value + ZetAspectsPlugin.ZetAspectRedStackDamageCfg.Value * (count - 1f);
-
-							// halve monster burn duration (and total damage output)
-							if (self.teamComponent.teamIndex != TeamIndex.Player) baseDuration *= ZetAspectsPlugin.ZetAspectEffectMonsterDamageMultCfg.Value;
-
-							// base damage over duration / 5 ticks
-							aspectDamage = baseDuration * damage;
+							if (self.teamComponent.teamIndex != TeamIndex.Player)
+							{
+								float monsterMult = ZetAspectsPlugin.ZetAspectEffectMonsterDamageMultCfg.Value;
+								aspectPercentBaseDamage *= monsterMult;
+								aspectDuration *= monsterMult;
+							}
 
 							// choose highest damage output
-							if (igniteDamage > aspectDamage)
+							if (ignitePercentBaseDamage > aspectPercentBaseDamage)
 							{
 								DotController.InflictDot(victim, damageInfo.attacker, DotController.DotIndex.PercentBurn, 4f * damageInfo.procCoefficient, 1f);
 							}
 							else
 							{
-								DotController.InflictDot(victim, damageInfo.attacker, DotController.DotIndex.PercentBurn, baseDuration, damage);
+								aspectDamageMult = aspectPercentBaseDamage / aspectDuration / 0.5f;
+								DotController.InflictDot(victim, damageInfo.attacker, DotController.DotIndex.PercentBurn, aspectDuration, aspectDamageMult);
 							}
+
+							return;
 						}
-						else
+
+						// Other Percent Burn
+						if ((damageInfo.damageType & DamageType.PercentIgniteOnHit) != DamageType.Generic)
 						{
-							if ((damageInfo.damageType & DamageType.PercentIgniteOnHit) != DamageType.Generic)
-							{
-								DotController.InflictDot(victim, damageInfo.attacker, DotController.DotIndex.PercentBurn, 4f * damageInfo.procCoefficient, 1f);
-							}
+							DotController.InflictDot(victim, damageInfo.attacker, DotController.DotIndex.PercentBurn, 4f * damageInfo.procCoefficient, 1f);
 						}
 					});
 				}
@@ -139,146 +330,10 @@ namespace TPDespair.ZetAspects
 					Debug.LogWarning("ZetAspect : Fire - DOT Application Hook Failed");
 				}
 			};
-
-			// Remove percent burn damage limit for players
-			IL.RoR2.DotController.AddDot += (il) =>
-			{
-				ILCursor c = new ILCursor(il);
-
-				bool found = c.TryGotoNext(
-					x => x.MatchCallvirt<HealthComponent>("get_fullCombinedHealth"),
-					x => x.MatchLdcR4(0.01f),
-					x => x.MatchMul(),
-					x => x.MatchCallOrCallvirt<Mathf>("Min")
-				);
-
-				if (found)
-				{
-					c.Index += 2;
-
-					c.Emit(OpCodes.Pop);
-					c.Emit(OpCodes.Ldloc_0);
-					c.EmitDelegate<Func<TeamIndex, float>>((teamIndex) =>
-					{
-						return teamIndex == TeamIndex.Player ? 1f : 0.01f;
-					});
-				}
-				else
-				{
-					Debug.LogWarning("ZetAspect : Fire - Burn Delimiter Hook Failed");
-				}
-			};
-
-			// Increase movement speed - additional jump
-			IL.RoR2.CharacterBody.RecalculateStats += (il) =>
-			{
-				ILCursor c = new ILCursor(il);
-
-				bool found = c.TryGotoNext(
-					x => x.MatchLdloc(53),
-					x => x.MatchLdloc(54),
-					x => x.MatchLdloc(55),
-					x => x.MatchDiv(),
-					x => x.MatchMul(),
-					x => x.MatchStloc(53)
-				);
-
-				if (found)
-				{
-					c.Index += 1;
-
-					c.Emit(OpCodes.Pop);
-
-					c.Emit(OpCodes.Ldarg, 0);
-					c.Emit(OpCodes.Ldloc, 54);
-					c.EmitDelegate<Func<CharacterBody, float, float>>((self, value) =>
-					{
-						if (self.HasBuff(BuffIndex.AffixRed))
-						{
-							float count = ZetAspectsPlugin.GetStackMagnitude(self, itemIndex);
-							float cap = ZetAspectsPlugin.ZetAspectRedMovementGainCapCfg.Value;
-							float addedSpeed = ZetAspectsPlugin.ZetAspectRedBaseMovementGainCfg.Value + ZetAspectsPlugin.ZetAspectRedStackMovementGainCfg.Value * (count - 1f);
-							if (cap > 0f) addedSpeed = Mathf.Min(addedSpeed, cap);
-							//if (self.teamComponent.teamIndex != TeamIndex.Player) addedSpeed *= 0.5f;
-							value += addedSpeed;
-						}
-
-						return value;
-					});
-					c.Emit(OpCodes.Stloc, 54);
-
-					c.Emit(OpCodes.Ldloc, 53);
-				}
-				else
-				{
-					Debug.LogWarning("ZetAspect : Fire - Movespeed Hook Failed");
-				}
-
-				found = c.TryGotoNext(
-					x => x.MatchLdarg(0),
-					x => x.MatchLdarg(0),
-					x => x.MatchLdfld<CharacterBody>("baseJumpCount"),
-					x => x.MatchLdloc(5)
-				);
-
-				if (found)
-				{
-					c.Emit(OpCodes.Ldarg, 0);
-					c.Emit(OpCodes.Ldloc, 5);
-					c.EmitDelegate<Func<CharacterBody, int, int>>((self, value) =>
-					{
-						if (self.HasBuff(BuffIndex.AffixRed))
-						{
-							if (ZetAspectsPlugin.ZetAspectRedExtraJumpCfg.Value)
-							{
-								if (self.teamComponent.teamIndex == TeamIndex.Player) value++;
-							}
-						}
-
-						return value;
-					});
-					c.Emit(OpCodes.Stloc, 5);
-				}
-				else
-				{
-					Debug.LogWarning("ZetAspect : Fire - Jump Hook Failed");
-				}
-			};
-
-			// Set Whether players leave fire trail
-			IL.RoR2.CharacterBody.UpdateFireTrail += (il) =>
-			{
-				ILCursor c = new ILCursor(il);
-
-				bool found = c.TryGotoNext(
-					x => x.MatchStloc(0)
-				);
-
-				if (found)
-				{
-					c.Index += 1;
-
-					c.Emit(OpCodes.Ldarg_0);
-					c.Emit(OpCodes.Ldloc, 0);
-					c.EmitDelegate<Func<CharacterBody, bool, bool>>((self, hasBuff) =>
-					{
-						if (self.teamComponent.teamIndex == TeamIndex.Player && !ZetAspectsPlugin.ZetAspectRedTrailCfg.Value) return false;
-
-						return hasBuff;
-					});
-					c.Emit(OpCodes.Stloc, 0);
-				}
-				else
-				{
-					Debug.LogWarning("ZetAspect : Fire - Firetrail Hook Failed");
-				}
-			};
 		}
 
-		private static float GetCritMult(CharacterBody characterBody,DamageInfo damageInfo)
+		private static float GetCritMult(CharacterBody characterBody)
 		{
-			if (!damageInfo.crit) return 1f;
-
 			if (characterBody && ZetAspectsPlugin.ZetHypercritEnabledCfg.Value)
 			{
 				//Amount of crit chance past 100%
