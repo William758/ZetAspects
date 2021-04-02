a39f60a4969539bfbe3054f38e52ee28ec02cf14
William758 <williamreyes758@gmail.com>
2021-04-01 21:18:59 -0700

    Update ZetAspectMalachite.cs

--- ZetAspectMalachite.cs
+++ ZetAspectMalachite.cs
@@ -1,5 +1,4 @@
 using RoR2;
-using R2API;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
 using System;
@@ -7,263 +6,93 @@ using UnityEngine;
 
 namespace TPDespair.ZetAspects
 {
-	public static class ZetAspectMalachite
-	{
-		public static ItemIndex itemIndex;
+    public static class ZetAspectMalachite
+    {
+		public static string identifier = "ZetAspectMalachite";
 
-		public static string nameToken = "ZETASPECTMALACHITE";
-
-		internal static void Init()
-		{
-			DefineItem();
-			DamageTakenHook();
-			NullDurationHook();
-			LifeGainHook();
-			HealthHook();
+		internal static void Hooks()
+        {
 			SpikeballHook();
-		}
+			NullDurationHook();
+        }
 
-		private static void DefineItem()
+		internal static ItemDef DefineItem()
 		{
-			string icon;
-
-			if (ZetAspectsPlugin.ZetAspectRedTierCfg.Value)
-			{
-				icon = "@ZetAspects:Assets/Import/icons/texAffixPoisonIconRed.png";
-			}
-			else
-			{
-				icon = "@ZetAspects:Assets/Import/icons/texAffixPoisonIconYellow.png";
-			}
-
 			ItemTag[] tags = { ItemTag.Healing, ItemTag.Utility };
-
-			if (!ZetAspectsPlugin.ZetAspectRedTierCfg.Value)
+			if (!Configuration.AspectRedTier.Value)
 			{
 				Array.Resize(ref tags, 3);
 				tags[2] = ItemTag.WorldUnique;
 			}
 
-			ItemDef itemDef = new ItemDef
+			Sprite sprite;
+			if (Configuration.AspectRedTier.Value)
 			{
-				name = "ITEM_" + nameToken,
-				nameToken = "ITEM_" + nameToken + "_NAME",
-				pickupToken = "ITEM_" + nameToken + "_PICKUP",
-				descriptionToken = "ITEM_" + nameToken + "_DESCRIPTION",
-				loreToken = "ITEM_" + nameToken + "_LORE",
-				pickupModelPath = "Prefabs/PickupModels/PickupAffixPoison",
-				pickupIconPath = icon,
-				tier = ZetAspectsPlugin.ZetAspectRedTierCfg.Value ? ItemTier.Tier3 : ItemTier.Boss,
-				tags = tags
-			};
+				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixPoisonIconRed.png");
+			}
+			else
+			{
+				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixPoisonIconYellow.png");
+			}
 
-			ItemDisplayRuleDict disp = new ItemDisplayRuleDict(null);
+			ItemDef itemDef = ScriptableObject.CreateInstance<ItemDef>();
+			itemDef.name = identifier;
+			itemDef.tags = tags;
+			itemDef.tier = Configuration.AspectRedTier.Value ? ItemTier.Tier3 : ItemTier.Boss;
+			itemDef.pickupModelPrefab = Resources.Load<GameObject>("Prefabs/PickupModels/PickupAffixPoison");
+			itemDef.pickupIconSprite = sprite;
 
-			itemIndex = ItemAPI.Add(new CustomItem(itemDef, disp));
+			itemDef.AutoPopulateTokens();
 
-			LanguageAPI.Add("ITEM_" + nameToken + "_NAME", "N'kuhana's Retort");
-			LanguageAPI.Add("ITEM_" + nameToken + "_PICKUP", "Become an aspect of corruption.");
-			LanguageAPI.Add("ITEM_" + nameToken + "_DESCRIPTION", BuildDescription());
-			LanguageAPI.Add("ITEM_" + nameToken + "_LORE", "...");
+			string locToken = identifier.ToUpperInvariant();
+
+			EnigmaticThunder.Modules.Languages.Add("ITEM_" + locToken + "_NAME", "N'kuhana's Retort");
+			EnigmaticThunder.Modules.Languages.Add("ITEM_" + locToken + "_PICKUP", "Become an aspect of corruption.");
+			EnigmaticThunder.Modules.Languages.Add("ITEM_" + locToken + "_DESC", BuildDescription());
+			EnigmaticThunder.Modules.Languages.Add("ITEM_" + locToken + "_LORE", "...");
+
+			return itemDef;
 		}
 
 		public static string BuildDescription()
 		{
 			string output = "<style=cDeath>Aspect of Corruption</style> :";
-			if (ZetAspectsPlugin.ZetAspectPoisonFireSpikesCfg.Value)
+			if (Configuration.AspectPoisonFireSpikes.Value)
 			{
 				output += "\nPeriodically releases spiked balls that sprout spike pits from where they land.";
 			}
 			output += "\nAttacks <style=cIsUtility>nullify</style> on hit for ";
-			output += ZetAspectsPlugin.FormatSeconds(ZetAspectsPlugin.ZetAspectPoisonNullDurationCfg.Value);
-			if (ZetAspectsPlugin.ZetAspectPoisonNullDamageTakenCfg.Value != 0f)
+			output += ZetAspectsPlugin.FormatSeconds(Configuration.AspectPoisonNullDuration.Value);
+			if (Configuration.AspectPoisonNullDamageTaken.Value != 0f)
 			{
 				output += ", increasing <style=cIsUtility>damage taken</style> by <style=cIsUtility>";
-				output += Mathf.Abs(ZetAspectsPlugin.ZetAspectPoisonNullDamageTakenCfg.Value) * 100f + "%</style>";
+				output += Mathf.Abs(Configuration.AspectPoisonNullDamageTaken.Value) * 100f + "%</style>";
 			}
 			output += ".";
-			if (ZetAspectsPlugin.ZetAspectPoisonBaseHealthGainCfg.Value > 0f)
+			if (Configuration.AspectPoisonBaseHealthGain.Value > 0f)
 			{
 				output += "\nIncrease <style=cIsHealing>maximum health</style> by <style=cIsHealing>";
-				output += ZetAspectsPlugin.ZetAspectPoisonBaseHealthGainCfg.Value + "</style>";
-				if (ZetAspectsPlugin.ZetAspectPoisonStackHealthGainCfg.Value != 0f)
+				output += Configuration.AspectPoisonBaseHealthGain.Value + "</style>";
+				if (Configuration.AspectPoisonStackHealthGain.Value != 0f)
 				{
 					output += " <style=cStack>(+";
-					output += ZetAspectsPlugin.ZetAspectPoisonStackHealthGainCfg.Value + " per stack)</style>";
+					output += Configuration.AspectPoisonStackHealthGain.Value + " per stack)</style>";
 				}
 				output += ".";
 			}
-			if (ZetAspectsPlugin.ZetAspectPoisonBaseLgohCfg.Value > 0)
+			if (Configuration.AspectPoisonBaseHeal.Value > 0)
 			{
 				output += "\nDealing damage <style=cIsHealing>heals</style> you for <style=cIsHealing>";
-				output += ZetAspectsPlugin.ZetAspectPoisonBaseLgohCfg.Value + "</style>";
-				if (ZetAspectsPlugin.ZetAspectPoisonStackLgohCfg.Value != 0)
+				output += Configuration.AspectPoisonBaseHeal.Value + "</style>";
+				if (Configuration.AspectPoisonStackHeal.Value != 0)
 				{
 					output += " <style=cStack>(+";
-					output += ZetAspectsPlugin.ZetAspectPoisonStackLgohCfg.Value + " per stack)</style>";
+					output += Configuration.AspectPoisonStackHeal.Value + " per stack)</style>";
 				}
-				output += " health.";
+				output += " <style=cIsHealing>health</style>.";
 			}
 
 			return output;
-		}
-
-		private static void DamageTakenHook()
-		{
-			IL.RoR2.HealthComponent.TakeDamage += (il) =>
-			{
-				ILCursor c = new ILCursor(il);
-
-				bool found = c.TryGotoNext(
-					x => x.MatchLdarg(1),
-					x => x.MatchLdfld<DamageInfo>("damage"),
-					x => x.MatchStloc(5)
-				);
-
-				if (found)
-				{
-					c.Index += 3;
-
-					c.Emit(OpCodes.Ldloc, 5);
-					c.Emit(OpCodes.Ldarg, 0);
-					c.EmitDelegate<Func<float, HealthComponent, float>>((damage, healthComponent) =>
-					{
-						if (healthComponent.body.HasBuff(BuffIndex.HealingDisabled) && ZetAspectsPlugin.ZetAspectPoisonNullDamageTakenCfg.Value != 0f)
-						{
-							float extraDamage = Mathf.Abs(ZetAspectsPlugin.ZetAspectPoisonNullDamageTakenCfg.Value);
-							if (healthComponent.body.teamComponent.teamIndex == TeamIndex.Player) extraDamage *= ZetAspectsPlugin.ZetAspectEffectPlayerDebuffMultCfg.Value;
-							return damage * (1f + extraDamage);
-						}
-						else
-						{
-							return damage;
-						}
-					});
-					c.Emit(OpCodes.Stloc, 5);
-				}
-				else
-				{
-					Debug.LogWarning("ZetAspect : Malachite - Nullified Damage Taken Hook Failed");
-				}
-			};
-		}
-
-		private static void NullDurationHook()
-		{
-			IL.RoR2.GlobalEventManager.OnHitEnemy += (il) =>
-			{
-				ILCursor c = new ILCursor(il);
-
-				bool found = c.TryGotoNext(
-					x => x.MatchLdloc(1),
-					x => x.MatchLdcI4(0x20),
-					x => x.MatchLdcR4(8f),
-					x => x.MatchLdarg(1),
-					x => x.MatchLdfld<DamageInfo>("procCoefficient"),
-					x => x.MatchMul()
-				);
-
-				if (found)
-				{
-					c.Index += 6;
-
-					c.Emit(OpCodes.Pop);
-					c.Emit(OpCodes.Ldloc_0);
-					c.Emit(OpCodes.Ldarg_1);
-					c.EmitDelegate<Func<CharacterBody, DamageInfo, float>>((self, damageInfo) =>
-					{
-						float duration = ZetAspectsPlugin.ZetAspectPoisonNullDurationCfg.Value;
-						if (self.teamComponent.teamIndex != TeamIndex.Player) duration = 8f * damageInfo.procCoefficient;
-
-						return duration;
-					});
-				}
-				else
-				{
-					Debug.LogWarning("ZetAspect : Malachite - Nullified Duration Hook Failed");
-				}
-			};
-		}
-
-		private static void LifeGainHook()
-		{
-			IL.RoR2.GlobalEventManager.OnHitEnemy += (il) =>
-			{
-				ILCursor c = new ILCursor(il);
-
-				// LGOH
-				bool found = c.TryGotoNext(
-					x => x.MatchLdloc(3),
-					x => x.MatchLdcI4(13),
-					x => x.MatchCallvirt<Inventory>("GetItemCount"),
-					x => x.MatchStloc(18)
-				);
-
-				if (found)
-				{
-					c.Index += 3;
-
-					c.Emit(OpCodes.Ldloc_0);
-					c.Emit(OpCodes.Ldloc, 18);
-					c.EmitDelegate<Func<int, CharacterBody, int, int>>((lgoh, self, seed) =>
-					{
-						if (ZetAspectsPlugin.ZetAspectLeechSeedLgohCfg.Value > 1)
-						{
-							lgoh += seed * (ZetAspectsPlugin.ZetAspectLeechSeedLgohCfg.Value - 1);
-						}
-
-						if (self.HasBuff(BuffIndex.AffixPoison) && ZetAspectsPlugin.ZetAspectPoisonBaseLgohCfg.Value > 0)
-						{
-							float count = ZetAspectsPlugin.GetStackMagnitude(self, itemIndex);
-							lgoh += ZetAspectsPlugin.ZetAspectPoisonBaseLgohCfg.Value + (int)(ZetAspectsPlugin.ZetAspectPoisonStackLgohCfg.Value * (count - 1f) + 0.55f);
-						}
-
-						return lgoh;
-					});
-				}
-				else
-				{
-					Debug.LogWarning("ZetAspect : Malachite - LGOH Hook Failed");
-				}
-			};
-		}
-
-		private static void HealthHook()
-		{
-			IL.RoR2.CharacterBody.RecalculateStats += (il) =>
-			{
-				ILCursor c = new ILCursor(il);
-
-				bool found = c.TryGotoNext(
-					x => x.MatchLdloc(41),
-					x => x.MatchLdloc(42),
-					x => x.MatchMul(),
-					x => x.MatchStloc(41)
-				);
-
-				if (found)
-				{
-					c.Emit(OpCodes.Ldarg_0);
-					c.Emit(OpCodes.Ldloc, 41);
-					c.EmitDelegate<Func<CharacterBody, float, float>>((self, health) =>
-					{
-						if (self.HasBuff(BuffIndex.AffixPoison) && ZetAspectsPlugin.ZetAspectPoisonBaseHealthGainCfg.Value > 0f)
-						{
-							float count = ZetAspectsPlugin.GetStackMagnitude(self, itemIndex);
-							float addedHealth = ZetAspectsPlugin.ZetAspectPoisonBaseHealthGainCfg.Value + ZetAspectsPlugin.ZetAspectPoisonStackHealthGainCfg.Value * (count - 1f);
-							//if (self.teamComponent.teamIndex != TeamIndex.Player) addedHealth *= 0.5f;
-							health += addedHealth;
-						}
-
-						return health;
-					});
-					c.Emit(OpCodes.Stloc, 41);
-				}
-				else
-				{
-					Debug.LogWarning("ZetAspect : Malachite - Health Hook Failed");
-				}
-			};
 		}
 
 		private static void SpikeballHook()
@@ -285,10 +114,10 @@ namespace TPDespair.ZetAspects
 				{
 					c.Index += 4;
 
-					c.Emit(OpCodes.Ldarg_0);
+					c.Emit(OpCodes.Ldarg, 0);
 					c.EmitDelegate<Func<float, CharacterBody, float>>((deltaTime, self) =>
 					{
-						if (self.teamComponent.teamIndex == TeamIndex.Player && !ZetAspectsPlugin.ZetAspectPoisonFireSpikesCfg.Value) return 0f;
+						if (self.teamComponent.teamIndex == TeamIndex.Player && !Configuration.AspectPoisonFireSpikes.Value) return 0f;
 
 						return deltaTime;
 					});
@@ -296,6 +125,43 @@ namespace TPDespair.ZetAspects
 				else
 				{
 					Debug.LogWarning("ZetAspect : Malachite - Spikeball Hook Failed");
+				}
+			};
+		}
+
+		private static void NullDurationHook()
+		{
+			IL.RoR2.GlobalEventManager.OnHitEnemy += (il) =>
+			{
+				ILCursor c = new ILCursor(il);
+
+				bool found = c.TryGotoNext(
+					x => x.MatchLdloc(1),
+					x => x.MatchLdsfld(typeof(RoR2Content.Buffs).GetField("HealingDisabled")),
+					x => x.MatchLdcR4(8f),
+					x => x.MatchLdarg(1),
+					x => x.MatchLdfld<DamageInfo>("procCoefficient"),
+					x => x.MatchMul()
+				);
+
+				if (found)
+				{
+					c.Index += 6;
+
+					c.Emit(OpCodes.Pop);
+					c.Emit(OpCodes.Ldloc, 0);
+					c.Emit(OpCodes.Ldarg, 1);
+					c.EmitDelegate<Func<CharacterBody, DamageInfo, float>>((self, damageInfo) =>
+					{
+						float duration = Configuration.AspectPoisonNullDuration.Value;
+						if (self.teamComponent.teamIndex != TeamIndex.Player) duration = 8f * damageInfo.procCoefficient;
+
+						return duration;
+					});
+				}
+				else
+				{
+					Debug.LogWarning("ZetAspect : Malachite - Nullified Duration Hook Failed");
 				}
 			};
 		}
