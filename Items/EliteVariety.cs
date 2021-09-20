6f5a54b727a54474f8b1db22939aedbfc618dcd1
William758 <williamreyes758@gmail.com>
2021-09-19 22:04:48 -0700

    Sprite And Text Setup

--- Items/EliteVariety.cs
+++ Items/EliteVariety.cs
@@ -17,47 +17,41 @@ namespace TPDespair.ZetAspects
 				tags[2] = ItemTag.WorldUnique;
 			}
 
-			Sprite sprite;
-			if (Configuration.AspectRedTier.Value)
-			{
-				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixArmoredIconRed.png");
-			}
-			else
-			{
-				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixArmoredIconYellow.png");
-			}
+			Sprite outlineSprite;
+			if (Configuration.AspectRedTier.Value) outlineSprite = ZetAspectsContent.Sprites.OutlineRed;
+			else outlineSprite = ZetAspectsContent.Sprites.OutlineYellow;
 
 			ItemDef itemDef = ScriptableObject.CreateInstance<ItemDef>();
 			itemDef.name = identifier;
 			itemDef.tags = tags;
 			itemDef.tier = Configuration.AspectRedTier.Value ? ItemTier.Tier3 : ItemTier.Boss;
 			itemDef.pickupModelPrefab = Resources.Load<GameObject>("Prefabs/PickupModels/PickupAffixWhite");
-			itemDef.pickupIconSprite = sprite;
+			itemDef.pickupIconSprite = ZetAspectsPlugin.CreateAspectSprite(ZetAspectsContent.Sprites.AffixArmored, outlineSprite);
 
 			itemDef.AutoPopulateTokens();
 
+			return itemDef;
+		}
+
+		public static void SetupTokens()
+		{
 			string locToken = identifier.ToUpperInvariant();
 
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_NAME", "Service for the Bulwark");
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_PICKUP", "Become an aspect of durability.");
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_DESC", BuildDescription());
-			//ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_LORE", "...");
+			Language.helperTarget = "default";
+			Language.RegisterToken("ITEM_" + locToken + "_NAME", "Service for the Bulwark");
+			Language.RegisterToken("ITEM_" + locToken + "_PICKUP", "Become an aspect of durability.");
 
-			return itemDef;
+			string desc = BuildDescription();
+			Language.RegisterToken("ITEM_" + locToken + "_DESC", desc);
+			Language.RegisterToken("EQUIPMENT_ELITEVARIETY_AFFIXARMORED_DESC", Language.EquipmentDescription(desc, "Gain temporary armor increase on use."));
 		}
 
 		public static string BuildDescription()
 		{
 			string output = "<style=cDeath>Aspect of Durability</style> :";
 			output += "\nSpawn with full barrier";
-			if (Catalog.DynamicBarrierDecay.slowed)
-			{
-				output += " that slowly decays";
-			}
-			else if (!Catalog.DynamicBarrierDecay.enabled)
-			{
-				output += " that doesn't decay";
-			}
+			if (Catalog.barrierDecayMode == 0) output += " that doesn't decay";
+			else if (Catalog.barrierDecayMode == 1) output += " that slowly decays";
 			output += ".\nAttacks <style=cIsUtility>root</style> on hit, immobilizing the target.";
 			if (Configuration.AspectArmorBaseArmorGain.Value > 0f)
 			{
@@ -65,8 +59,7 @@ namespace TPDespair.ZetAspects
 				output += Configuration.AspectArmorBaseArmorGain.Value + "</style>";
 				if (Configuration.AspectArmorStackArmorGain.Value != 0f)
 				{
-					output += " <style=cStack>(+";
-					output += Configuration.AspectArmorStackArmorGain.Value + " per stack)</style>";
+					output += " " + Language.StackText(Configuration.AspectArmorStackArmorGain.Value);
 				}
 				output += ".";
 			}
@@ -90,47 +83,46 @@ namespace TPDespair.ZetAspects
 				tags[2] = ItemTag.WorldUnique;
 			}
 
-			Sprite sprite;
-			if (Configuration.AspectRedTier.Value)
-			{
-				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixBuffingIconRed.png");
-			}
-			else
-			{
-				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixBuffingIconYellow.png");
-			}
+			Sprite outlineSprite;
+			if (Configuration.AspectRedTier.Value) outlineSprite = ZetAspectsContent.Sprites.OutlineRed;
+			else outlineSprite = ZetAspectsContent.Sprites.OutlineYellow;
 
 			ItemDef itemDef = ScriptableObject.CreateInstance<ItemDef>();
 			itemDef.name = identifier;
 			itemDef.tags = tags;
 			itemDef.tier = Configuration.AspectRedTier.Value ? ItemTier.Tier3 : ItemTier.Boss;
 			itemDef.pickupModelPrefab = Resources.Load<GameObject>("Prefabs/PickupModels/PickupAffixWhite");
-			itemDef.pickupIconSprite = sprite;
+			itemDef.pickupIconSprite = ZetAspectsPlugin.CreateAspectSprite(ZetAspectsContent.Sprites.AffixBuffing, outlineSprite);
 
 			itemDef.AutoPopulateTokens();
 
+			return itemDef;
+		}
+
+		public static void SetupTokens()
+		{
 			string locToken = identifier.ToUpperInvariant();
 
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_NAME", "Retaliation of the Planet");
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_PICKUP", "Become an aspect of battle.");
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_DESC", BuildDescription());
-			//ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_LORE", "...");
+			Language.helperTarget = "default";
+			Language.RegisterToken("ITEM_" + locToken + "_NAME", "Retaliation of the Planet");
+			Language.RegisterToken("ITEM_" + locToken + "_PICKUP", "Become an aspect of battle.");
 
-			return itemDef;
+			string desc = BuildDescription();
+			Language.RegisterToken("ITEM_" + locToken + "_DESC", desc);
+			Language.RegisterToken("EQUIPMENT_ELITEVARIETY_AFFIXBUFFING_DESC", Language.EquipmentDescription(desc, "Increase banner radius on use."));
 		}
 
 		public static string BuildDescription()
 		{
 			string output = "<style=cDeath>Aspect of Battle</style> :";
-			output += "\nGrants allies inside its spherical aura increased <style=cIsDamage>movement speed</style> and <style=cIsDamage>attack speed</style>.";
+			output += "\nGrants nearby allies increased <style=cIsDamage>movement speed</style> and <style=cIsDamage>attack speed</style>.";
 			if (Configuration.AspectBannerBaseAttackSpeedGain.Value > 0f)
 			{
 				output += "\nIncreases <style=cIsUtility>attack speed</style> by <style=cIsUtility>";
 				output += Configuration.AspectBannerBaseAttackSpeedGain.Value * 100f + "%</style>";
 				if (Configuration.AspectBannerStackAttackSpeedGain.Value != 0f)
 				{
-					output += " <style=cStack>(+";
-					output += Configuration.AspectBannerStackAttackSpeedGain.Value * 100f + "% per stack)</style>";
+					output += " " + Language.StackText(Configuration.AspectBannerStackAttackSpeedGain.Value * 100f, "", "%");
 				}
 				output += ".";
 			}
@@ -154,33 +146,33 @@ namespace TPDespair.ZetAspects
 				tags[2] = ItemTag.WorldUnique;
 			}
 
-			Sprite sprite;
-			if (Configuration.AspectRedTier.Value)
-			{
-				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixImpPlaneIconRed.png");
-			}
-			else
-			{
-				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixImpPlaneIconYellow.png");
-			}
+			Sprite outlineSprite;
+			if (Configuration.AspectRedTier.Value) outlineSprite = ZetAspectsContent.Sprites.OutlineRed;
+			else outlineSprite = ZetAspectsContent.Sprites.OutlineYellow;
 
 			ItemDef itemDef = ScriptableObject.CreateInstance<ItemDef>();
 			itemDef.name = identifier;
 			itemDef.tags = tags;
 			itemDef.tier = Configuration.AspectRedTier.Value ? ItemTier.Tier3 : ItemTier.Boss;
 			itemDef.pickupModelPrefab = Resources.Load<GameObject>("Prefabs/PickupModels/PickupAffixWhite");
-			itemDef.pickupIconSprite = sprite;
+			itemDef.pickupIconSprite = ZetAspectsPlugin.CreateAspectSprite(ZetAspectsContent.Sprites.AffixImpPlane, outlineSprite);
 
 			itemDef.AutoPopulateTokens();
 
+			return itemDef;
+		}
+
+		public static void SetupTokens()
+		{
 			string locToken = identifier.ToUpperInvariant();
 
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_NAME", "Invasion from the Red Plane");
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_PICKUP", "Become an aspect of violence.");
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_DESC", BuildDescription());
-			//ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_LORE", "...");
+			Language.helperTarget = "default";
+			Language.RegisterToken("ITEM_" + locToken + "_NAME", "Invasion from the Red Plane");
+			Language.RegisterToken("ITEM_" + locToken + "_PICKUP", "Become an aspect of violence.");
 
-			return itemDef;
+			string desc = BuildDescription();
+			Language.RegisterToken("ITEM_" + locToken + "_DESC", desc);
+			Language.RegisterToken("EQUIPMENT_ELITEVARIETY_AFFIXIMPPLANE_DESC", Language.EquipmentDescription(desc, "Teleport to a target and deal damage on use."));
 		}
 
 		public static string BuildDescription()
@@ -194,8 +186,7 @@ namespace TPDespair.ZetAspects
 				output += Configuration.AspectImpaleBaseDotAmp.Value * 100f + "%</style>";
 				if (Configuration.AspectImpaleStackDotAmp.Value != 0f)
 				{
-					output += " <style=cStack>(+";
-					output += Configuration.AspectImpaleStackDotAmp.Value * 100f + "% per stack)</style>";
+					output += " " + Language.StackText(Configuration.AspectImpaleStackDotAmp.Value * 100f, "", "%");
 				}
 				output += ".";
 			}
@@ -219,33 +210,33 @@ namespace TPDespair.ZetAspects
 				tags[2] = ItemTag.WorldUnique;
 			}
 
-			Sprite sprite;
-			if (Configuration.AspectRedTier.Value)
-			{
-				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixPillagingIconRed.png");
-			}
-			else
-			{
-				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixPillagingIconYellow.png");
-			}
+			Sprite outlineSprite;
+			if (Configuration.AspectRedTier.Value) outlineSprite = ZetAspectsContent.Sprites.OutlineRed;
+			else outlineSprite = ZetAspectsContent.Sprites.OutlineYellow;
 
 			ItemDef itemDef = ScriptableObject.CreateInstance<ItemDef>();
 			itemDef.name = identifier;
 			itemDef.tags = tags;
 			itemDef.tier = Configuration.AspectRedTier.Value ? ItemTier.Tier3 : ItemTier.Boss;
 			itemDef.pickupModelPrefab = Resources.Load<GameObject>("Prefabs/PickupModels/PickupAffixWhite");
-			itemDef.pickupIconSprite = sprite;
+			itemDef.pickupIconSprite = ZetAspectsPlugin.CreateAspectSprite(ZetAspectsContent.Sprites.AffixPillaging, outlineSprite);
 
 			itemDef.AutoPopulateTokens();
 
+			return itemDef;
+		}
+
+		public static void SetupTokens()
+		{
 			string locToken = identifier.ToUpperInvariant();
 
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_NAME", "Curse of Greed");
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_PICKUP", "Become an aspect of theft.");
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_DESC", BuildDescription());
-			//ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_LORE", "...");
+			Language.helperTarget = "default";
+			Language.RegisterToken("ITEM_" + locToken + "_NAME", "Curse of Greed");
+			Language.RegisterToken("ITEM_" + locToken + "_PICKUP", "Become an aspect of theft.");
 
-			return itemDef;
+			string desc = BuildDescription();
+			Language.RegisterToken("ITEM_" + locToken + "_DESC", desc);
+			Language.RegisterToken("EQUIPMENT_ELITEVARIETY_AFFIXPILLAGING_DESC", Language.EquipmentDescription(desc, "Spend all of your gold for a random item. The more gold spent, the higher chance of getting a rarer item."));
 		}
 
 		public static string BuildDescription()
@@ -258,8 +249,7 @@ namespace TPDespair.ZetAspects
 				output += Configuration.AspectGoldenBaseRegenGain.Value + "hp/s</style>";
 				if (Configuration.AspectGoldenStackRegenGain.Value != 0f)
 				{
-					output += " <style=cStack>(+";
-					output += Configuration.AspectGoldenStackRegenGain.Value + "hp/s per stack)</style>";
+					output += " " + Language.StackText(Configuration.AspectGoldenStackRegenGain.Value, "", "hp/s");
 				}
 				output += ".";
 			}
@@ -270,8 +260,7 @@ namespace TPDespair.ZetAspects
 				output += Configuration.AspectGoldenBaseScoredRegenGain.Value * 100f + "%</style>";
 				if (Configuration.AspectGoldenStackScoredRegenGain.Value != 0f)
 				{
-					output += " <style=cStack>(+";
-					output += Configuration.AspectGoldenStackScoredRegenGain.Value * 100f + "% per stack)</style>";
+					output += " " + Language.StackText(Configuration.AspectGoldenStackScoredRegenGain.Value * 100f, "", "%");
 				}
 				output += ".";
 			}
@@ -295,33 +284,33 @@ namespace TPDespair.ZetAspects
 				tags[2] = ItemTag.WorldUnique;
 			}
 
-			Sprite sprite;
-			if (Configuration.AspectRedTier.Value)
-			{
-				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixSandstormIconRed.png");
-			}
-			else
-			{
-				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixSandstormIconYellow.png");
-			}
+			Sprite outlineSprite;
+			if (Configuration.AspectRedTier.Value) outlineSprite = ZetAspectsContent.Sprites.OutlineRed;
+			else outlineSprite = ZetAspectsContent.Sprites.OutlineYellow;
 
 			ItemDef itemDef = ScriptableObject.CreateInstance<ItemDef>();
 			itemDef.name = identifier;
 			itemDef.tags = tags;
 			itemDef.tier = Configuration.AspectRedTier.Value ? ItemTier.Tier3 : ItemTier.Boss;
 			itemDef.pickupModelPrefab = Resources.Load<GameObject>("Prefabs/PickupModels/PickupAffixWhite");
-			itemDef.pickupIconSprite = sprite;
+			itemDef.pickupIconSprite = ZetAspectsPlugin.CreateAspectSprite(ZetAspectsContent.Sprites.AffixSandstorm, outlineSprite);
 
 			itemDef.AutoPopulateTokens();
 
+			return itemDef;
+		}
+
+		public static void SetupTokens()
+		{
 			string locToken = identifier.ToUpperInvariant();
 
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_NAME", "The Third Wish");
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_PICKUP", "Become an aspect of sand.");
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_DESC", BuildDescription());
-			//ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_LORE", "...");
+			Language.helperTarget = "default";
+			Language.RegisterToken("ITEM_" + locToken + "_NAME", "The Third Wish");
+			Language.RegisterToken("ITEM_" + locToken + "_PICKUP", "Become an aspect of sand.");
 
-			return itemDef;
+			string desc = BuildDescription();
+			Language.RegisterToken("ITEM_" + locToken + "_DESC", desc);
+			Language.RegisterToken("EQUIPMENT_ELITEVARIETY_AFFIXSANDSTORM_DESC", Language.EquipmentDescription(desc, "Dash on use, knocking nearby enemies up."));
 		}
 
 		public static string BuildDescription()
@@ -335,8 +324,7 @@ namespace TPDespair.ZetAspects
 				output += Configuration.AspectCycloneBaseMovementGain.Value * 100f + "%</style>";
 				if (Configuration.AspectCycloneStackMovementGain.Value != 0f)
 				{
-					output += " <style=cStack>(+";
-					output += Configuration.AspectCycloneStackMovementGain.Value * 100f + "% per stack)</style>";
+					output += " " + Language.StackText(Configuration.AspectCycloneStackMovementGain.Value * 100f, "", "%");
 				}
 				output += ".";
 			}
@@ -360,33 +348,33 @@ namespace TPDespair.ZetAspects
 				tags[2] = ItemTag.WorldUnique;
 			}
 
-			Sprite sprite;
-			if (Configuration.AspectRedTier.Value)
-			{
-				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixTinkererIconRed.png");
-			}
-			else
-			{
-				sprite = ZetAspectsPlugin.Assets.LoadAsset<Sprite>("Assets/Icons/texAffixTinkererIconYellow.png");
-			}
+			Sprite outlineSprite;
+			if (Configuration.AspectRedTier.Value) outlineSprite = ZetAspectsContent.Sprites.OutlineRed;
+			else outlineSprite = ZetAspectsContent.Sprites.OutlineYellow;
 
 			ItemDef itemDef = ScriptableObject.CreateInstance<ItemDef>();
 			itemDef.name = identifier;
 			itemDef.tags = tags;
 			itemDef.tier = Configuration.AspectRedTier.Value ? ItemTier.Tier3 : ItemTier.Boss;
 			itemDef.pickupModelPrefab = Resources.Load<GameObject>("Prefabs/PickupModels/PickupAffixWhite");
-			itemDef.pickupIconSprite = sprite;
+			itemDef.pickupIconSprite = ZetAspectsPlugin.CreateAspectSprite(ZetAspectsContent.Sprites.AffixTinkerer, outlineSprite);
 
 			itemDef.AutoPopulateTokens();
 
+			return itemDef;
+		}
+
+		public static void SetupTokens()
+		{
 			string locToken = identifier.ToUpperInvariant();
 
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_NAME", "Neural Link");
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_PICKUP", "Become an aspect of Automatization.");
-			ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_DESC", BuildDescription());
-			//ZetAspectsPlugin.RegisterLanguageToken("ITEM_" + locToken + "_LORE", "...");
+			Language.helperTarget = "default";
+			Language.RegisterToken("ITEM_" + locToken + "_NAME", "Neural Link");
+			Language.RegisterToken("ITEM_" + locToken + "_PICKUP", "Become an aspect of Automatization.");
 
-			return itemDef;
+			string desc = BuildDescription();
+			Language.RegisterToken("ITEM_" + locToken + "_DESC", desc);
+			Language.RegisterToken("EQUIPMENT_ELITEVARIETY_AFFIXTINKERER_DESC", Language.EquipmentDescription(desc, "Heal drones on use."));
 		}
 
 		public static string BuildDescription()
@@ -400,8 +388,7 @@ namespace TPDespair.ZetAspects
 				output += Configuration.AspectTinkerBaseDamageResistGain.Value * 100f + "%</style>";
 				if (Configuration.AspectTinkerStackDamageResistGain.Value != 0f)
 				{
-					output += " <style=cStack>(+";
-					output += Configuration.AspectTinkerStackDamageResistGain.Value * 100f + "% per stack)</style>";
+					output += " " + Language.StackText(Configuration.AspectTinkerStackDamageResistGain.Value * 100f, "", "%");
 				}
 				output += ".";
 			}
