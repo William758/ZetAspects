ed1a7fd3656b37a35117421d8014f1dfd9f9705d
William758 <williamreyes758@gmail.com>
2021-11-09 00:17:29 -0800

    Blight Elite and Activation Effects

--- Items/LostInTransit.cs
+++ Items/LostInTransit.cs
@@ -44,7 +44,7 @@ namespace TPDespair.ZetAspects
 			string desc = BuildDescription();
 			Language.RegisterToken("ITEM_" + locToken + "_DESC", desc);
 			if (!DropHooks.CanObtainItem()) desc = BuildEquipmentDescription();
-			Language.RegisterToken("LIT_EQUIP_AFFIXLEECHING_DESC", Language.EquipmentDescription(desc, "- ? Activation Effect ? -"));
+			Language.RegisterToken("LIT_EQUIP_AFFIXLEECHING_DESC", Language.EquipmentDescription(desc, "Restore nearby allies's health."));
 		}
 
 		public static string BuildDescription()
@@ -66,7 +66,8 @@ namespace TPDespair.ZetAspects
 			}
 			else
 			{
-				output += "\n<style=cIsHealing>Heal</style> for <style=cIsHealing>100%</style> of the <style=cIsDamage>damage</style> you deal.";
+				float value = LostInTransitHooks.leechMatchedIndex == 1 ? 100f : 50f;
+				output += "\n<style=cIsHealing>Heal</style> for <style=cIsHealing>" + value + "%</style> of the <style=cIsDamage>damage</style> you deal.";
 			}
 
 			return output;
@@ -90,7 +91,8 @@ namespace TPDespair.ZetAspects
 			}
 			else
 			{
-				output += "\n<style=cIsHealing>Heal</style> for <style=cIsHealing>100%</style> of the <style=cIsDamage>damage</style> you deal.";
+				float value2 = LostInTransitHooks.leechMatchedIndex == 1 ? 100f : 50f;
+				output += "\n<style=cIsHealing>Heal</style> for <style=cIsHealing>" + value2 + "%</style> of the <style=cIsDamage>damage</style> you deal.";
 			}
 
 			return output;
@@ -137,7 +139,7 @@ namespace TPDespair.ZetAspects
 			string desc = BuildDescription();
 			Language.RegisterToken("ITEM_" + locToken + "_DESC", desc);
 			if (!DropHooks.CanObtainItem()) desc = BuildEquipmentDescription();
-			Language.RegisterToken("LIT_EQUIP_AFFIXFRENZIED_DESC", Language.EquipmentDescription(desc, "- ? Activation Effect ? -"));
+			Language.RegisterToken("LIT_EQUIP_AFFIXFRENZIED_DESC", Language.EquipmentDescription(desc, "Reduced Cooldowns on Use."));
 		}
 
 		public static string BuildDescription()
@@ -339,7 +341,7 @@ namespace TPDespair.ZetAspects
 			string desc = BuildDescription();
 			Language.RegisterToken("ITEM_" + locToken + "_DESC", desc);
 			if (!DropHooks.CanObtainItem()) desc = BuildEquipmentDescription();
-			Language.RegisterToken("LIT_EQUIP_AFFIXVOLATILE_DESC", Language.EquipmentDescription(desc, "- ? Activation Effect ? -"));
+			Language.RegisterToken("LIT_EQUIP_AFFIXVOLATILE_DESC", Language.EquipmentDescription(desc, "Charge a Mini Explosion that harms nearby enemies."));
 		}
 
 		public static string BuildDescription()
@@ -388,6 +390,144 @@ namespace TPDespair.ZetAspects
 			else
 			{
 				output += "\nAttacks <style=cIsDamage>explode</style> on hit, dealing <style=cIsDamage>30%</style> TOTAL damage.";
+			}
+
+			return output;
+		}
+	}
+
+	public static class ZetAspectBlighted
+	{
+		public static string identifier = "ZetAspectBlighted";
+
+		internal static ItemDef DefineItem()
+		{
+			ItemTag[] tags = { ItemTag.Healing, ItemTag.Damage, ItemTag.Utility };
+			if (Configuration.AspectWorldUnique.Value)
+			{
+				Array.Resize(ref tags, 4);
+				tags[3] = ItemTag.WorldUnique;
+			}
+
+			Sprite outlineSprite;
+			if (Configuration.AspectRedTier.Value) outlineSprite = ZetAspectsContent.Sprites.OutlineRed;
+			else outlineSprite = ZetAspectsContent.Sprites.OutlineYellow;
+
+			ItemDef itemDef = ScriptableObject.CreateInstance<ItemDef>();
+			itemDef.name = identifier;
+			itemDef.tags = tags;
+			itemDef.tier = Configuration.AspectRedTier.Value ? ItemTier.Tier3 : ItemTier.Boss;
+			itemDef.pickupModelPrefab = Resources.Load<GameObject>("Prefabs/PickupModels/PickupAffixWhite");
+			itemDef.pickupIconSprite = ZetAspectsPlugin.CreateAspectSprite(ZetAspectsContent.Sprites.AffixBlighted, outlineSprite);
+
+			itemDef.AutoPopulateTokens();
+
+			return itemDef;
+		}
+
+		public static void SetupTokens()
+		{
+			string locToken = identifier.ToUpperInvariant();
+
+			Language.helperTarget = "default";
+			Language.RegisterToken("ITEM_" + locToken + "_NAME", "Betrayal of the Bulwark");
+			Language.RegisterToken("ITEM_" + locToken + "_PICKUP", "Become another misguided soul.");
+
+			string desc = BuildDescription();
+			Language.RegisterToken("ITEM_" + locToken + "_DESC", desc);
+			if (!DropHooks.CanObtainItem()) desc = BuildEquipmentDescription();
+			Language.RegisterToken("LIT_EQUIP_AFFIXBLIGHTED_DESC", Language.EquipmentDescription(desc, "Reshuffle your 2 Elite Buffs."));
+		}
+
+		public static string BuildDescription()
+		{
+			string output = "<style=cDeath>Misguided Soul</style> :";
+			output += "\nGain 2 random Elite Affixes.";
+
+			if (LostInTransitHooks.blightStatControl)
+			{
+				if (Configuration.AspectBlightedBaseHealthGain.Value > 0f)
+				{
+					output += "\nIncreases <style=cIsHealing>maximum health</style> by <style=cIsHealing>";
+					output += Configuration.AspectBlightedBaseHealthGain.Value * 100f + "%</style>";
+					if (Configuration.AspectBlightedStackHealthGain.Value != 0f)
+					{
+						output += " " + Language.StackText(Configuration.AspectBlightedStackHealthGain.Value * 100f, "", "%");
+					}
+					output += ".";
+				}
+				if (Configuration.AspectBlightedBaseDamageGain.Value > 0f)
+				{
+					output += "\nIncreases <style=cIsDamage>damage</style> by <style=cIsDamage>";
+					output += Configuration.AspectBlightedBaseDamageGain.Value * 100f + "%</style>";
+					if (Configuration.AspectBlightedStackDamageGain.Value != 0f)
+					{
+						output += " " + Language.StackText(Configuration.AspectBlightedStackDamageGain.Value * 100f, "", "%");
+					}
+					output += ".";
+				}
+			}
+
+			if (LostInTransitHooks.blightCDRHook == 4)
+			{
+				if (Configuration.AspectBlightedBaseCooldownGain.Value != 0f)
+				{
+					output += "\nReduces <style=cIsUtility>skill cooldowns</style> by <style=cIsUtility>";
+					output += Mathf.Abs(Configuration.AspectBlightedBaseCooldownGain.Value) * 100f + "%</style>";
+					if (Configuration.AspectBlightedStackCooldownGain.Value != 0f)
+					{
+						output += " " + Language.StackText(Mathf.Abs(Configuration.AspectBlightedStackCooldownGain.Value) * 100f, "", "%");
+					}
+					output += ".";
+				}
+			}
+			else
+			{
+				output += "\nGreatly Reduces <style=cIsUtility>skill cooldowns</style>.";
+			}
+
+			return output;
+		}
+
+		public static string BuildEquipmentDescription()
+		{
+			float value, stacks = Mathf.Max(1f, Configuration.AspectEquipmentEffect.Value);
+
+			string output = "<style=cDeath>Misguided Soul</style> :";
+			output += "\nGain 2 random Elite Affixes.";
+
+			if (LostInTransitHooks.blightStatControl)
+			{
+				if (Configuration.AspectBlightedBaseHealthGain.Value > 0f)
+				{
+					value = Configuration.AspectBlightedBaseHealthGain.Value + Configuration.AspectBlightedStackHealthGain.Value * (stacks - 1f);
+					output += "\nIncreases <style=cIsHealing>maximum health</style> by <style=cIsHealing>";
+					output += value * 100f + "%</style>.";
+				}
+				if (Configuration.AspectBlightedBaseDamageGain.Value > 0f)
+				{
+					value = Configuration.AspectBlightedBaseDamageGain.Value + Configuration.AspectBlightedStackDamageGain.Value * (stacks - 1f);
+					output += "\nIncreases <style=cIsDamage>damage</style> by <style=cIsDamage>";
+					output += value * 100f + "%</style>.";
+				}
+			}
+			else
+			{
+				output += "\nGreatly Increases <style=cIsUtility>stats</style>.";
+			}
+
+			if (LostInTransitHooks.blightCDRHook == 4)
+			{
+				if (Configuration.AspectBlightedBaseCooldownGain.Value != 0f)
+				{
+					value = Mathf.Abs(Configuration.AspectBlightedBaseCooldownGain.Value) + Mathf.Abs(Configuration.AspectBlightedStackCooldownGain.Value) * (stacks - 1f);
+					output += "\nReduces <style=cIsUtility>skill cooldowns</style> by <style=cIsUtility>";
+					output += value * 100f + "%</style>.";
+				}
+			}
+			else
+			{
+				output += "\nGreatly Reduces <style=cIsUtility>skill cooldowns</style>.";
 			}
 
 			return output;
