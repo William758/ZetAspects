d1e8d78d130f211d6e83ea4172e0b41bf546eedc
William758 <williamreyes758@gmail.com>
2021-12-28 19:29:17 -0800

    Update Effects And Descriptions

--- Items/EliteVariety.cs
+++ Items/EliteVariety.cs
@@ -136,17 +136,25 @@ namespace TPDespair.ZetAspects
 		public static string BuildDescription()
 		{
 			string output = "<style=cDeath>Aspect of Battle</style> :";
-			output += "\nGrants nearby allies increased <style=cIsUtility>movement speed</style> and <style=cIsUtility>attack speed</style>.";
-			if (Configuration.AspectBannerBaseAttackSpeedGain.Value > 0f)
+			output += "\nCarry a warbanner that empowers nearby allies.";
+
+			if (Configuration.AspectBannerExtraJump.Value)
 			{
-				output += "\nIncreases <style=cIsUtility>attack speed</style> by <style=cIsUtility>";
-				output += Configuration.AspectBannerBaseAttackSpeedGain.Value * 100f + "%</style>";
-				if (Configuration.AspectBannerStackAttackSpeedGain.Value != 0f)
+				output += "\nGain <style=cIsUtility>+1</style> maximum <style=cIsUtility>jump count</style>.";
+			}
+
+			if (Configuration.AspectBannerBaseDamageGain.Value > 0f)
+			{
+				output += "\nIncreases <style=cIsDamage>damage</style> by <style=cIsDamage>";
+				output += Configuration.AspectBannerBaseDamageGain.Value * 100f + "%</style>";
+				if (Configuration.AspectBannerStackDamageGain.Value != 0f)
 				{
-					output += " " + Language.StackText(Configuration.AspectBannerStackAttackSpeedGain.Value * 100f, "", "%");
+					output += " " + Language.StackText(Configuration.AspectBannerStackDamageGain.Value * 100f, "", "%");
 				}
 				output += ".";
 			}
+
+			output += "\nGrants you and nearby allies increased <style=cIsUtility>movement speed</style> and <style=cIsDamage>attack speed</style>.";
 
 			return output;
 		}
@@ -156,13 +164,21 @@ namespace TPDespair.ZetAspects
 			float value, stacks = Mathf.Max(1f, Configuration.AspectEquipmentEffect.Value);
 
 			string output = "<style=cDeath>Aspect of Battle</style> :";
-			output += "\nGrants nearby allies increased <style=cIsUtility>movement speed</style> and <style=cIsUtility>attack speed</style>.";
-			if (Configuration.AspectBannerBaseAttackSpeedGain.Value > 0f)
+			output += "\nCarry a warbanner that empowers nearby allies.";
+
+			if (Configuration.AspectBannerExtraJump.Value)
 			{
-				value = Configuration.AspectBannerBaseAttackSpeedGain.Value + Configuration.AspectBannerStackAttackSpeedGain.Value * (stacks - 1f);
-				output += "\nIncreases <style=cIsUtility>attack speed</style> by <style=cIsUtility>";
+				output += "\nGain <style=cIsUtility>+1</style> maximum <style=cIsUtility>jump count</style>.";
+			}
+
+			if (Configuration.AspectBannerBaseDamageGain.Value > 0f)
+			{
+				value = Configuration.AspectBannerBaseDamageGain.Value + Configuration.AspectBannerStackDamageGain.Value * (stacks - 1f);
+				output += "\nIncreases <style=cIsDamage>damage</style> by <style=cIsDamage>";
 				output += value * 100f + "%</style>.";
 			}
+
+			output += "\nGrants you and nearby allies increased <style=cIsUtility>movement speed</style> and <style=cIsDamage>attack speed</style>.";
 
 			return output;
 		}
@@ -216,11 +232,11 @@ namespace TPDespair.ZetAspects
 		public static string BuildDescription()
 		{
 			string output = "<style=cDeath>Aspect of Violence</style> :";
-			output += "\nEnemies looking directly at you are <style=cIsUtility>marked</style>, increasing damage taken.";
-			output += "\nAttacks <style=cIsDamage>impale</style> on hit, periodically dealing heavy damage over 60 seconds.";
+			output += "\nEnemies looking directly at you are <style=cIsDamage>marked</style>, increasing <style=cIsDamage>damage taken</style>.";
+			output += "\nAttacks <style=cIsDamage>impale</style> on hit, periodically dealing heavy damage.";
 			if (Configuration.AspectImpaleBaseDotAmp.Value > 0f)
 			{
-				output += "\nIncreases <style=cIsUtility>damage over time multiplier</style> by <style=cIsUtility>";
+				output += "\nIncreases <style=cIsDamage>damage over time multiplier</style> by <style=cIsDamage>";
 				output += Configuration.AspectImpaleBaseDotAmp.Value * 100f + "%</style>";
 				if (Configuration.AspectImpaleStackDotAmp.Value != 0f)
 				{
@@ -237,12 +253,12 @@ namespace TPDespair.ZetAspects
 			float value, stacks = Mathf.Max(1f, Configuration.AspectEquipmentEffect.Value);
 
 			string output = "<style=cDeath>Aspect of Violence</style> :";
-			output += "\nEnemies looking directly at you are <style=cIsUtility>marked</style>, increasing damage taken.";
-			output += "\nAttacks <style=cIsDamage>impale</style> on hit, periodically dealing heavy damage over 60 seconds.";
+			output += "\nEnemies looking directly at you are <style=cIsDamage>marked</style>, increasing <style=cIsDamage>damage taken</style>.";
+			output += "\nAttacks <style=cIsDamage>impale</style> on hit, periodically dealing heavy damage.";
 			if (Configuration.AspectImpaleBaseDotAmp.Value > 0f)
 			{
 				value = Configuration.AspectImpaleBaseDotAmp.Value + Configuration.AspectImpaleStackDotAmp.Value * (stacks - 1f);
-				output += "\nIncreases <style=cIsUtility>damage over time multiplier</style> by <style=cIsUtility>";
+				output += "\nIncreases <style=cIsDamage>damage over time multiplier</style> by <style=cIsDamage>";
 				output += value * 100f + "%</style>.";
 			}
 
@@ -394,16 +410,65 @@ namespace TPDespair.ZetAspects
 		{
 			string output = "<style=cDeath>Aspect of Sand</style> :";
 			output += "\nSurrounded by a sandstorm that damages enemies on contact.";
-			output += "\nAttacks <style=cIsUtility>blind</style> on hit, reducing visibility.";
-			if (Configuration.AspectCycloneBaseMovementGain.Value > 0f)
+
+			output += "\nAttacks <style=cIsUtility>blind</style> on hit";
+			if (Configuration.AspectCycloneBlindDodgeEffect.Value > 0f)
 			{
-				output += "\nIncreases <style=cIsUtility>movement speed</style> by <style=cIsUtility>";
-				output += Configuration.AspectCycloneBaseMovementGain.Value * 100f + "%</style>";
-				if (Configuration.AspectCycloneStackMovementGain.Value != 0f)
+				output += ", reducing visibility and hit-chance.";
+			}
+			else
+			{
+				output += ", reducing visibility.";
+			}
+
+			if (Configuration.AspectCycloneBaseDodgeGain.Value > 0f)
+			{
+				output += "\n<style=cIsHealing>";
+				output += Configuration.AspectCycloneBaseDodgeGain.Value * 100f + "%</style>";
+				if (Configuration.AspectCycloneStackDodgeGain.Value != 0f)
 				{
-					output += " " + Language.StackText(Configuration.AspectCycloneStackMovementGain.Value * 100f, "", "%");
+					output += " " + Language.StackText(Configuration.AspectCycloneStackDodgeGain.Value * 100f, "", "%");
+				}
+				output += " chance to <style=cIsHealing>dodge</style> incoming damage. <style=cIsUtility>Unaffected by luck</style>.";
+			}
+
+			float cycloneBaseMS = Configuration.AspectCycloneBaseMovementGain.Value;
+			float cycloneStackMS = Configuration.AspectCycloneStackMovementGain.Value;
+			float cycloneBaseAS = Configuration.AspectCycloneBaseAttackSpeedGain.Value;
+			float cycloneStackAS = Configuration.AspectCycloneStackAttackSpeedGain.Value;
+
+			if (cycloneBaseMS == cycloneBaseAS && cycloneStackMS == cycloneStackAS)
+			{
+				output += "\nIncreases <style=cIsUtility>movement speed</style> and <style=cIsDamage>attack speed</style> by <style=cIsDamage>";
+				output += cycloneBaseMS * 100f + "%</style>";
+				if (cycloneStackMS != 0f)
+				{
+					output += " " + Language.StackText(cycloneStackMS * 100f, "", "%");
 				}
 				output += ".";
+			}
+			else
+			{
+				if (cycloneBaseMS > 0f)
+				{
+					output += "\nIncreases <style=cIsUtility>movement speed</style> by <style=cIsUtility>";
+					output += cycloneBaseMS * 100f + "%</style>";
+					if (cycloneStackMS != 0f)
+					{
+						output += " " + Language.StackText(cycloneStackMS * 100f, "", "%");
+					}
+					output += ".";
+				}
+				if (cycloneBaseAS > 0f)
+				{
+					output += "\nIncreases <style=cIsDamage>attack speed</style> by <style=cIsDamage>";
+					output += cycloneBaseAS * 100f + "%</style>";
+					if (cycloneStackAS != 0f)
+					{
+						output += " " + Language.StackText(cycloneStackAS * 100f, "", "%");
+					}
+					output += ".";
+				}
 			}
 
 			return output;
@@ -415,12 +480,49 @@ namespace TPDespair.ZetAspects
 
 			string output = "<style=cDeath>Aspect of Sand</style> :";
 			output += "\nSurrounded by a sandstorm that damages enemies on contact.";
-			output += "\nAttacks <style=cIsUtility>blind</style> on hit, reducing visibility.";
-			if (Configuration.AspectCycloneBaseMovementGain.Value > 0f)
+
+			output += "\nAttacks <style=cIsUtility>blind</style> on hit";
+			if (Configuration.AspectCycloneBlindDodgeEffect.Value > 0f)
 			{
-				value = Configuration.AspectCycloneBaseMovementGain.Value + Configuration.AspectCycloneStackMovementGain.Value * (stacks - 1f);
-				output += "\nIncreases <style=cIsUtility>movement speed</style> by <style=cIsUtility>";
+				output += ", reducing visibility and hit-chance.";
+			}
+			else
+			{
+				output += ", reducing visibility.";
+			}
+
+			if (Configuration.AspectCycloneBaseDodgeGain.Value > 0f)
+			{
+				value = Configuration.AspectCycloneBaseDodgeGain.Value + Configuration.AspectCycloneStackDodgeGain.Value * (stacks - 1f);
+				output += "\n<style=cIsHealing>" + value * 100f + "%</style>";
+				output += " chance to <style=cIsHealing>dodge</style> incoming damage. <style=cIsUtility>Unaffected by luck</style>.";
+			}
+
+			float cycloneBaseMS = Configuration.AspectCycloneBaseMovementGain.Value;
+			float cycloneStackMS = Configuration.AspectCycloneStackMovementGain.Value;
+			float cycloneBaseAS = Configuration.AspectCycloneBaseAttackSpeedGain.Value;
+			float cycloneStackAS = Configuration.AspectCycloneStackAttackSpeedGain.Value;
+
+			if (cycloneBaseMS == cycloneBaseAS && cycloneStackMS == cycloneStackAS)
+			{
+				value = cycloneBaseMS + cycloneStackMS * (stacks - 1f);
+				output += "\nIncreases <style=cIsUtility>movement speed</style> and <style=cIsDamage>attack speed</style> by <style=cIsDamage>";
 				output += value * 100f + "%</style>.";
+			}
+			else
+			{
+				if (cycloneBaseMS > 0f)
+				{
+					value = cycloneBaseMS + cycloneStackMS * (stacks - 1f);
+					output += "\nIncreases <style=cIsUtility>movement speed</style> by <style=cIsUtility>";
+					output += value * 100f + "%</style>.";
+				}
+				if (cycloneBaseAS > 0f)
+				{
+					value = cycloneBaseAS + cycloneStackAS * (stacks - 1f);
+					output += "\nIncreases <style=cIsDamage>attack speed</style> by <style=cIsDamage>";
+					output += value * 100f + "%</style>.";
+				}
 			}
 
 			return output;
@@ -500,6 +602,16 @@ namespace TPDespair.ZetAspects
 
 				if (!EliteVarietyHooks.tinkerStealHook) output += "\nAttacks steal scrap from the victim.";
 			}
+			if (Configuration.AspectTinkerBaseDamageGain.Value > 0f)
+			{
+				output += "\nDrones deal <style=cIsDamage>";
+				output += Configuration.AspectTinkerBaseDamageGain.Value * 100f + "%</style>";
+				if (Configuration.AspectTinkerStackDamageGain.Value != 0f)
+				{
+					output += " " + Language.StackText(Configuration.AspectTinkerStackDamageGain.Value * 100f, "", "%");
+				}
+				output += " increased <style=cIsDamage>damage</style>.";
+			}
 			if (Configuration.AspectTinkerBaseDamageResistGain.Value > 0f)
 			{
 				output += "\nDrones have <style=cIsHealing>damage taken</style> reduced by <style=cIsHealing>";
@@ -543,6 +655,12 @@ namespace TPDespair.ZetAspects
 				}
 
 				if (!EliteVarietyHooks.tinkerStealHook) output += "\nAttacks steal scrap from the victim.";
+			}
+			if (Configuration.AspectTinkerBaseDamageGain.Value > 0f)
+			{
+				value = Configuration.AspectTinkerBaseDamageGain.Value + Configuration.AspectTinkerStackDamageGain.Value * (stacks - 1f);
+				output += "\nDrones deal <style=cIsDamage>";
+				output += value * 100f + "%</style> increased <style=cIsDamage>damage</style>.";
 			}
 			if (Configuration.AspectTinkerBaseDamageResistGain.Value > 0f)
 			{
