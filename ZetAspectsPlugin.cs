1f964af565f1e0b929d6654a68c08dd447244630
William758 <williamreyes758@gmail.com>
2021-12-28 19:25:52 -0800

    Update ZetAspectsPlugin.cs

--- ZetAspectsPlugin.cs
+++ ZetAspectsPlugin.cs
@@ -22,7 +22,7 @@ namespace TPDespair.ZetAspects
 
 	public class ZetAspectsPlugin : BaseUnityPlugin
 	{
-		public const string ModVer = "2.6.1";
+		public const string ModVer = "2.6.2";
 		public const string ModName = "ZetAspects";
 		public const string ModGuid = "com.TPDespair.ZetAspects";
 
